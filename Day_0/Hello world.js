a436892a988f0743b4b42a23407e5006c4528ad5
huska0707 <hussaink001haled@gmail.com>
2024-08-01 19:29:57 -0500

    Hello world

---
+++ Day_0/Hello world.js
@@ -0,0 +1,6 @@
+function greeting(parameterVariable) {
+  console.log("Hello World!");
+  console.log(parameterVariable);
+
+  //Write a line of code that prints parameterVariable to stdout using console.log
+}
